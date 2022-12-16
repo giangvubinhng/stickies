f8c9d43c0feee549116e710632f30f7fa339f473
Giang Nguyen <giangvubinhn@vt.edu>
2022-12-16 15:43:07 -0500

    edit working, not properly

--- pages/stickies.tsx
+++ pages/stickies.tsx
@@ -8,9 +8,10 @@ import { ICard, ICardInput } from '@/interfaces/ICard'
 import AddCardModal from '@/components/AddCardModal';
 import { unstable_getServerSession } from 'next-auth';
 import { authOptions } from './api/auth/[...nextauth]';
-import cardApiService from '@/services/api/cardApi.service'
+import cardApiService from '@/services/api/cardApi.service';
 import { ServerResponse } from '@/interfaces/api/IAPI';
 import cardsService from '@/services/cards.service';
+import AlertMessage from '@/components/AlertMessage';
 
 
 interface props {
@@ -40,27 +41,48 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
 const Stickies: NextPage<props> = ({ result }) => {
   const [currCards, setCurrCards] = useState<ICard[]>([])
   const [showModal, setShowModal] = useState(false)
+  const [alertMessage, setAlertMessage] = useState({
+    state: false,
+    message: '',
+    type: ''
+  })
   const cards: ICard[] = useCardStore((state) => state.cards);
+
+  function toggleMessage(message: string, type?: string) {
+
+    setAlertMessage({ state: true, message: message, type: type || '' })
+    setTimeout(() => {
+      setAlertMessage({ state: false, message: '', type: '' })
+    }, 5000)
+  }
 
   const addCardAsync = async (task: ICardInput) => {
     const data = await cardsService.addCardAsync(task);
-    if (data.success) {
+    if (data.success === true) {
       setCurrCards([...currCards, data.data])
+      toggleMessage(data.message, 'info')
+    }
+    else {
+      toggleMessage(data.message, 'error')
     }
 
   }
 
   const updateCardAsync = async (obj: any) => {
     const data = await cardsService.updateCardAsync(obj.card, obj.id);
-    if (data.success) {
+    if (data.success === true) {
       currCards.forEach((card) => {
-        if(card.id == data.data.id){
+        if (card.id == data.data.id) {
           card = data.data
         }
       })
       setCurrCards(currCards)
-
+      toggleMessage(data.message, 'info')
     }
+    else {
+      toggleMessage(data.message, 'error')
+    }
+
 
   }
   useEffect(
@@ -76,7 +98,8 @@ const Stickies: NextPage<props> = ({ result }) => {
 
   return (
     <div>
-      <Cards cards={currCards} updateCardAsyncProp={updateCardAsync}/>
+      {alertMessage.state && <AlertMessage type={alertMessage.type} message={alertMessage.message} />}
+      <Cards cards={currCards} updateCardAsyncProp={updateCardAsync} />
       {showModal && <AddCardModal setShowModal={(e) => setShowModal(e)} addCardAsync={addCardAsync} />}
       <RightFloatingBtn onClick={() => setShowModal(true)} />
     </div>
